cd69b076a26be907a0cec2567530d4f4f52931dc
Miguel Angel Ajo <majopela@redhat.com>
2018-10-17 21:34:26 +0200

    More sequences

--- firmware/src/jelly.ino
+++ firmware/src/jelly.ino
@@ -18,6 +18,7 @@ void setup() {
   setup_display();
   setup_ota_upgrades();
 
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -27,23 +28,15 @@ void setup() {
     ArduinoOTA.handle();
     delay(25);
   }
-
+  
 }
 
-void loop() {
 
-  static float f=0.0;
-  float brightness=200.0;
-  
+void animation_1(float f, float brightness) {
+
   float fs,fp;
   float R,G,B;
   int s,p;
-
-  clear_display();
-
-  f=f-0.04;
-
-  if (f<-314.15) f=314.15;
 
   for (s=0;s<8;s++)
   for (p=0;p<7;p++) {
@@ -60,18 +53,77 @@ void loop() {
 
     jelly_pixel(s, p, color(brightness*fabs(R), brightness*fabs(G), brightness*fabs(B)));
 
+  }
+  
+}
+void animation_2(float f, float brightness) {
+
+  float fs,fp;
+  float R,G,B;
+  int s,p;
+
+  for (s=0;s<8;s++)
+  for (p=0;p<7;p++) {
+
+    fp = p; // height (top to bottom)
+    fs = s; // angle
+
+    fp=(fp/7)*3.1415+f;
+    fs=(fs/8)*3.1415;
+
+    R = sin(fp)+1.0;
+    G = cos(fp*1.2)+1.0;
+    B = sin(fp*1.4)+1.0;
+
+    jelly_pixel(s, p, color(brightness*fabs(R), brightness*fabs(G), brightness*fabs(B)));
 
   }
   
-  if (p>=7) {
-      p=0;
-      s+=1;
-  } 
-  if (s>=8) {
-      s= 0;
+}
+void animation_3(float f, float brightness) {
+
+  float f_bright;
+  float fs,fp;
+  float R,G,B;
+  int s,p;
+
+  for (s=0;s<8;s++)
+  for (p=0;p<7;p++) {
+
+    fp = p; // height (top to bottom)
+    fs = s; // angle
+
+    fp=(fp/7)*3.1415;
+    fs=(fs/8)*3.1415;
+
+    R = sin(fp+f)+1.0;
+    G = cos((fp+f)*1.2)+1.0;
+    B = sin((fp+f)*1.4)+1.0;
+
+    f_bright = brightness * (sin((fp*3.0)+f*0.5));
+
+    if (f_bright<0.0) f_bright=0.0;
+
+    jelly_pixel(s, p, color(f_bright*fabs(R), f_bright*fabs(G), f_bright*fabs(B)));
+
   }
+  
+}
+
+void loop() {
+
+  static float f=0.0;
+  float brightness=100.0;
+  
+  f=f-0.04;
+  if (f<-314.15) f=314.15;
+
+  clear_display();
+
+  animation_3(f, brightness);
 
   show_display();
-
+ 
   ArduinoOTA.handle();
+
 }
